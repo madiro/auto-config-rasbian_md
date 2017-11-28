b11393b98c13c5a66047ee420254ade721f010c8
madiro <manuel.dr.email@gmail.com>
2017-11-28 12:02:05 +0100

    Delete scriptbackground.sh

--- scriptbackground.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-sudo sed -i 's/wallpaper_mode=.*/wallpaper_mode=color/'   /home/pi/.config/pcmanfm/LXDE-pi/desktop-items-0.conf
-sudo sed -i 's/desktop_bg=#.*/desktop_bg=#000000000000/' /home/pi/.config/pcmanfm/LXDE-pi/desktop-items-0.conf
-
