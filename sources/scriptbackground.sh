e651c9397be9423976e3c941d32b60398b7ddc8a
madiro <manuel.dr.email@gmail.com>
2017-11-28 12:31:23 +0100

    Update scriptbackground.sh

--- sources/scriptbackground.sh
+++ sources/scriptbackground.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-sudo sed -i 's/wallpaper_mode=.*/wallpaper_mode=color/'   /home/pi/.config/pcmanfm/LXDE-pi/desktop-items-0.conf
-sudo sed -i 's/desktop_bg=#.*/desktop_bg=#000000000000/' /home/pi/.config/pcmanfm/LXDE-pi/desktop-items-0.conf
+sudo sed -i 's/wallpaper_mode=.*/wallpaper_mode=color/'   /home/$USER/.config/pcmanfm/LXDE-pi/desktop-items-0.conf
+sudo sed -i 's/desktop_bg=#.*/desktop_bg=#000000000000/' /home/$USER/.config/pcmanfm/LXDE-pi/desktop-items-0.conf
 echo "[ OK ] Fondo de pantalla"
